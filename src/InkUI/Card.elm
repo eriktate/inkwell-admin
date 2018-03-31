9c44d3a431d72c0c185c7b520ca5aa06322bb998
eriktate <erik.tate.r@gmail.com>
2018-03-31 11:48:51 -0400

    First stab at updating Card.elm to new elm-css format

--- src/InkUI/Card.elm
+++ src/InkUI/Card.elm
@@ -1,87 +1,66 @@
 module InkUI.Card exposing (..)
 
-import Html exposing (..)
+import Html.Styled exposing (..)
 import Html.Attributes exposing (style)
 import Css exposing (..)
+import Css.Foreign exposing (children, everything)
 import InkUI.Base exposing (..)
 import InkUI.Grid exposing (inkRow, inkCol)
-import Html.CssHelpers
 
 
-{ id, class, classList } =
-    Html.CssHelpers.withNamespace namespace
-
-
-type CssClasses
-    = Card
-    | Title
-    | Body
-    | Menu
-
-
-css : List Snippet
-css =
-    [ Css.class Card
+inkCard : List (Attribute msg) -> List (Html msg) -> Html msg
+inkCard =
+    styled div
         [ backgroundColor ultrawhite
         , borderRadius (px 2)
         , boxShadow4 (px 2) (px 4) (px 5) gray
         , margin (Css.em 1)
         , padding (Css.em 1)
-        , descendants
-            [ Css.class Title
-                [ borderBottom3 (px 1) solid black
-                , margin4 (Css.em -1) (Css.em -1) (px 0) (Css.em -1)
-                , padding (Css.em 1)
-                , backgroundColor black
+        ]
+
+
+inkCardTitle : List (Attribute msg) -> List (Html msg) -> Html msg
+inkCardTitle =
+    styled div
+        [ borderBottom3 (px 1) solid black
+        , margin4 (Css.em -1) (Css.em -1) (px 0) (Css.em -1)
+        , padding (Css.em 1)
+        , backgroundColor black
+        , color white
+        , fontWeight bold
+        ]
+
+
+inkCardBody : List (Attribute msg) -> List (Html msg) -> Html msg
+inkCardBody =
+    styled inkRow [ marginTop (Css.em 1) [] ]
+
+
+inkCardMenuBody : List (Attribute msg) -> List (Html msg) -> List (Html msg) -> Html msg
+inkCardMenuBody attrs body menu =
+    inkCardBody
+        attrs
+        [ inkCol 11 [] [ body ]
+        , inkCol 1 [] [ menu ]
+        ]
+
+
+inkCardMenu : List (Attribute msg) -> List (Html msg) -> Html msg
+inkCardMenu =
+    styled div
+        [ margin4 (px 0) (Css.em -1) (Css.em -1) (px 0)
+        , backgroundColor dark
+        , minWidth (px 48)
+        , maxWidth (px 48)
+        , right (px 0)
+        , height (pct 100)
+        , children
+            [ everything
+                [ display block
+                , textAlign center
+                , padding (px 8)
                 , color white
-                , fontWeight bold
+                , fontSize (pt 18)
                 ]
-            , Css.class Body
-                [ marginTop (Css.em 1) ]
-            , Css.class Menu
-                [ margin4 (px 0) (Css.em -1) (Css.em -1) (px 0)
-                , backgroundColor dark
-                , minWidth (px 48)
-                , maxWidth (px 48)
-                , right (px 0)
-                , height (pct 100)
-                , children
-                    [ everything
-                        [ display block
-                        , textAlign center
-                        , padding (px 8)
-                        , color white
-                        , fontSize (pt 18)
-                        ]
-                    ]
-                ]
-            ]
-        ]
-    ]
-
-
-inkCard : List (Attribute msg) -> Html msg -> Html msg
-inkCard attrs body =
-    div (class [ Card ] :: attrs)
-        [ body ]
-
-
-inkCardWithTitle : List (Attribute msg) -> Html msg -> Html msg -> Html msg
-inkCardWithTitle attrs title body =
-    div (class [ Card ] :: attrs)
-        [ title
-        , body
-        ]
-
-
-inkCardWithMenu : List (Attribute msg) -> Html msg -> Html msg -> List (Html msg) -> Html msg
-inkCardWithMenu attrs title body menu =
-    div (class [ Card ] :: attrs)
-        [ title
-        , inkRow []
-            [ inkCol 11 [] [ body ]
-            , inkCol 1
-                [ class [ Menu ] ]
-                menu
             ]
         ]
